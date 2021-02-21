bb3988eaff2b376857ca5f29ba98182f5ff227f6
Partha Sarthy <parthasarthyiitvns@gmail.com>
2021-02-21 21:50:26 +0000

    FInal

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    var v = validate_principal();
+    if (v == 0)
+    return;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = (principal * years * rate)/100;
     var amount = +principal + +interest;
     var curr_year = new Date().getFullYear();
     var result = document.getElementById("result");
-    result.innerHTML = "If you deposit " + principal;
-    result.innerHTML += ",at an interest rate of "+rate+"%.\n You will receive an amount of "+amount+",\nin the year "+(+curr_year + +years);
+    result.innerHTML = "If you deposit " + "<mark>"+principal+"</mark>";
+    result.innerHTML += ",at an interest rate of "+"<mark>"+rate+"%"+"</mark>"+". You will receive an amount of "+"<mark>"+amount+"</mark>"+",\nin the year "+"<mark>"+(+curr_year + +years)+"</mark>";
     
 }
 function display_rate()
@@ -19,11 +22,10 @@ function display_rate()
 }
 function validate_principal()
 {
-    var p =  document.getElementById("principal").value;
-    if (p <= 0)
-    {
-        var output = document.getElementById("valid-principal");
-        output.innerHTML = "Enter a positive number";
-    }
+    var p = document.getElementById("principal").value;
+    if (p < 0) {
+        alert("Enter a positive number");
+        return 0;
+    } else return 1;
 }
         
