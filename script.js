94d835e1e5017fe524621f4e1e196b5c2f4a26ca
Partha Sarthy <parthasarthyiitvns@gmail.com>
2021-02-21 20:32:46 +0000

    milestone 2

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function compute()
 {
-    principal = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-    years = document.getElementById("years").value;
-    interest = (principal * years * rate)/100;
-    amount = principal + interest;
-    curr_year = new Date().getFullYear();
-    result = document.getElementById("result");
-    result.innerHTML = "If you deposit " + principal + "at an interest rate of "+rate+"%. You will receive an amount of "+amount+"in the year "+(curr_year+years);
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (principal * years * rate)/100;
+    var amount = +principal + +interest;
+    var curr_year = new Date().getFullYear();
+    var result = document.getElementById("result");
+    result.innerHTML = "If you deposit " + principal;
+    result.innerHTML += ",at an interest rate of "+rate+"%.\n You will receive an amount of "+amount+",\nin the year "+(+curr_year + +years);
     
 }
 function display_rate()
 {
-    slider = document.getElementById("rate");
-    output = document.getElementById("rate-op");
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate-op");
     output.innerHTML = slider.value;
 }
         
